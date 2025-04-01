c76680a41acd10f71d9397600969c77f6988626c
mannuccimattia <mannuccimattia@gmail.com>
2025-04-01 19:17:25 +0200

    [UPD] Done thumbnails sync with gallery

--- js/script.js
+++ js/script.js
@@ -1,39 +1,39 @@
 const pics = [
-    {
-      image: 'img/01.jpg',
-      title: 'Svezia',
-      text: 'Scandinavia\'s blend of nature and innovation.',
-    }, 
-    {
-      image: 'img/02.jpg',
-      title: 'Norvegia',
-      text: 'Fjords, mountains, and coastal charm in Nordic splendor.',
-    }, 
-    {
-      image: 'img/03.jpg',
-      title: 'Alaska',
-      text: 'Untamed wilderness and rugged beauty in the Last Frontier.',
-    }, 
-    {
-      image: 'img/04.jpg',
-      title: 'Gran Canyon',
-      text: 'Nature\'s masterpiece, a colorful tapestry of cliffs.',
-    }, 
-    {
-      image: 'img/05.jpg',
-      title: "Skyrim",
-      text: 'Epic Nordic fantasy with dragons and ancient magic.',
-    }
-  ];
+  {
+    image: 'img/01.jpg',
+    title: 'Svezia',
+    text: 'Scandinavia\'s blend of nature and innovation.',
+  },
+  {
+    image: 'img/02.jpg',
+    title: 'Norvegia',
+    text: 'Fjords, mountains, and coastal charm in Nordic splendor.',
+  },
+  {
+    image: 'img/03.jpg',
+    title: 'Alaska',
+    text: 'Untamed wilderness and rugged beauty in the Last Frontier.',
+  },
+  {
+    image: 'img/04.jpg',
+    title: 'Gran Canyon',
+    text: 'Nature\'s masterpiece, a colorful tapestry of cliffs.',
+  },
+  {
+    image: 'img/05.jpg',
+    title: "Skyrim",
+    text: 'Epic Nordic fantasy with dragons and ancient magic.',
+  }
+];
 
 //   DEFINIZIONE DELLE FUNZIONI
 // definizione della funzione che crea la singola immagine
-  const createImage = (galleryImage) => {
-    // destrutturiamo il parametro passato nelle sue proprietà
-    const { image, title, text } = galleryImage;
+const createImage = (galleryImage) => {
+  // destrutturiamo il parametro passato nelle sue proprietà
+  const { image, title, text } = galleryImage;
 
-    // credo l'html dell'immagine da inserire
-    let img = `<figure>
+  // credo l'html dell'immagine da inserire
+  let img = `<figure>
                     <img src="./${image}" alt="">
                     <figcaption>
                         <h2>${title}</h2>
@@ -41,9 +41,9 @@ const pics = [
                     </figcaption>
                 </figure>`;
 
-    return img;
+  return img;
 
-  }
+}
 
 // funzione che mi crea un thumbnail
 const createThumb = (galleryImage) => {
@@ -59,22 +59,22 @@ const createThumb = (galleryImage) => {
 
   return img;
 
-}  
+}
 
 //   definizione della funzione che va a ciclare l'array ed a creare le immagini a partire da questo nell'html
 const renderImages = (array) => {
 
-    // creo la variabile vuota che mi servirà per contenere tutte le immagini
-    let images = '';
-    // recupero la gallery dal dom
-    const gallery = document.querySelector('.gallery');
+  // creo la variabile vuota che mi servirà per contenere tutte le immagini
+  let images = '';
+  // recupero la gallery dal dom
+  const gallery = document.querySelector('.gallery');
 
-    // ciclo l'array
-    for(let i=0; i<array.length; i++){
-        images += createImage(array[i]);
-    }
+  // ciclo l'array
+  for (let i = 0; i < array.length; i++) {
+    images += createImage(array[i]);
+  }
 
-    gallery.innerHTML = images;
+  gallery.innerHTML = images;
 }
 
 // definizione della funzione che va a ciclare l'array ed a creare i thumbnails a partire da questo nell'html 
@@ -86,8 +86,8 @@ const renderThumb = (array) => {
   const gallery = document.querySelector('#thumbnails .row');
 
   // ciclo l'array
-  for(let i=0; i<array.length; i++){
-      images += createThumb(array[i]);
+  for (let i = 0; i < array.length; i++) {
+    images += createThumb(array[i]);
   }
 
   gallery.innerHTML = images;
@@ -95,36 +95,43 @@ const renderThumb = (array) => {
 
 // funzione che mi manda avanti le immagini da vedere
 const nextImage = () => {
-    // rimuovo la classe active dall'elemento attualmente attivo
-    images[activeImage].classList.remove('active');
-    // incremento il valore del cursore
-    activeImage++;
+  // rimuovo la classe active dall'elemento attualmente attivo
+  images[activeImage].classList.remove('active');
+  thumbs[activeImage].classList.remove('bright');
+  thumbs[activeImage].classList.add('dark');
+  // incremento il valore del cursore
+  activeImage++;
 
-    // se il cursore ha un valore maggiore rispetto alla lunghezza dell'array lo rimetto a zero
-    if(activeImage >= images.length){
-        activeImage = 0;
-    }
-    
-    // imposto la classe active al nuovo elemento
-    images[activeImage].classList.add('active');
+  // se il cursore ha un valore maggiore rispetto alla lunghezza dell'array lo rimetto a zero
+  if (activeImage >= images.length) {
+    activeImage = 0;
+  }
 
-    
+  // imposto la classe active al nuovo elemento
+  images[activeImage].classList.add('active');
+  thumbs[activeImage].classList.remove('dark');
+  thumbs[activeImage].classList.add('bright');
+
 }
 
 // funzione che mi manda indietro le immagini da vedere
 const previousImage = () => {
-    // rimuovo la classe active dall'elemento attualmente attivo
-    images[activeImage].classList.remove('active');
-    // decremento il valore del cursore
-    activeImage--;
+  // rimuovo la classe active dall'elemento attualmente attivo
+  images[activeImage].classList.remove('active');
+  thumbs[activeImage].classList.remove('bright');
+  thumbs[activeImage].classList.add('dark');
+  // decremento il valore del cursore
+  activeImage--;
 
-    // se il cursore ha un valore minore di zero lo imposto all'ultimo elemento dell'array
-    if(activeImage < 0){
-        activeImage = images.length - 1;
-    }
+  // se il cursore ha un valore minore di zero lo imposto all'ultimo elemento dell'array
+  if (activeImage < 0) {
+    activeImage = images.length - 1;
+  }
 
-    // imposto la classe active al nuovo elemento
-    images[activeImage].classList.add('active');
+  // imposto la classe active al nuovo elemento
+  images[activeImage].classList.add('active');
+  thumbs[activeImage].classList.remove('dark');
+  thumbs[activeImage].classList.add('bright');
 
 }
 
@@ -142,7 +149,7 @@ let activeImage = 0;
 // // vado a prendere tutte le immagini dal dom
 const images = document.querySelectorAll('#carousel figure');
 // recupero i thumbnail dal dom
-const thumbs = document.querySelectorAll('#thumbnails .col figure img'); 
+const thumbs = document.querySelectorAll('#thumbnails .col figure img');
 // aggiungo all'elemento di gallery con indice 0 dell'array images la classe active
 images[activeImage].classList.add('active');
 // aggiungo all'elemento di thumbnails con indice 0 dell'array images la classe bright e rimuovo la classe dark
